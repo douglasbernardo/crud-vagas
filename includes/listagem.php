69c9fcb995d3caab111b2377478a8a137fa09281
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2021-06-09 19:23:47 -0300

    tabela das vagas

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,40 +1,25 @@
+
 <main>
     <section>
-        <a href="index.php">
-            <button class="btn btn-success">
-                Voltar
-            </button>
+        <a href="cadastrar.php">
+            <button type="submit" class="btn btn-success"> Nova Vaga</button>
         </a>
     </section>
 
-    <h2 class="mt-3">Cadastrar Vaga</h2>
-
-    <form action="cadastrar.php" method="post">
-        <div class="form-group">
-            <label for="titulo">Titulo</label>
-            <input type="text" class="form-control" name="titulo">
-        </div>
-        <div class="form-group">
-            <label for="descricao">Descrição</label>
-            <textarea type="text" class="form-control" name="descricao" rows="6"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="status">Status</label>
-            <div>
-                <div class="form-check form-check-inline">
-                    <label class="form-control">
-                        <input type="radio" name="ativo" value="s" checked> Ativo
-                    </label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <label class="form-control">
-                        <input type="radio" name="ativo" value="n"> Inativo
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-success">Cadastrar</button>
-        </div>
-    </form>
+    <section>
+        <table class="table bg-light mt-3">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Descrição</th>
+                    <th>Status</th>
+                    <th>Data</th>
+                </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+        </table>
+    </section>
 </main>
