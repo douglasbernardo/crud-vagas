05ab3e14bce131c90f216d3e6058894575fc874d
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2021-06-19 23:34:49 -0300

    adicionando estilos com css

--- includes/header.php
+++ includes/header.php
@@ -7,12 +7,12 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../app/css/css.css">
     <title>Crud Vagas</title>
   </head>
   <body class="bg-dark text-light">
         <div class="container">
             <div class="junbotron bg-danger">
-                <h1>Vagas DEV</h1>
-                <p>Crud para cadastro de vagas de empregos</p>
+                <h1 class="display-4">Vagas de Empregos</h1>
+                <p class="lead">Crud para cadastro de vagas de empregos</p>
             </div>
